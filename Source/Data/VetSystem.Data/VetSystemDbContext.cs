c296fa046a8ba0d118170f07e29724b42a7e6133
g-yonchev <georgi.d.yonchev@gmail.com>
2016-02-13 15:51:52 +0200

    Separated User and DbContext from Web project to Data.Models project and Data project

---
+++ Source/Data/VetSystem.Data/VetSystemDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace VetSystem.Data
+{
+	using Microsoft.AspNet.Identity.EntityFramework;
+
+	using VetSystem.Data.Models;
+
+	public class VetSystemDbContext : IdentityDbContext<User>
+	{
+		public VetSystemDbContext()
+			: base("DefaultConnection", throwIfV1Schema: false)
+		{
+		}
+
+		public static VetSystemDbContext Create()
+		{
+			return new VetSystemDbContext();
+		}
+	}
+}
